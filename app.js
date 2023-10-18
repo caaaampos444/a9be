911d3b0a5fd668b4f3b5dc5b3c7deaa9d591c2fd
GUSTAVO DE CAMPOS <23111788@CFP127ALUNO.LOCAL>
2023-10-18 12:22:29 -0300

    exercicio 1 finalizado

---
+++ app.js
@@ -0,0 +1,30 @@
+const estadosCidades=require('./modulo/estados_cidades')
+const getListaDeEstados=function(){
+    let JSONSiglas={}
+    let arraySiglas=[]
+    let contador=0
+    while(contador<27){
+        arraySiglas.push(estadosCidades.estados[contador].sigla)
+        contador++
+    }
+    JSONSiglas.uf=arraySiglas
+    JSONSiglas.quantidade=arraySiglas.length
+    return JSONSiglas
+}
+const getDadosEstado=function(){
+    let JSONDados={}
+    let filtro='RJ'
+    let contador=0
+    loop: while(true){
+            if(filtro==estadosCidades.estados[contador].sigla){
+                JSONDados.uf=estadosCidades.estados[contador].sigla
+                JSONDados.descricao=estadosCidades.estados[contador].nome
+                JSONDados.capital=estadosCidades.estados[contador].capital
+                JSONDados.regiao=estadosCidades.estados[contador].regiao
+                break loop
+            }
+    }
+    return JSONDados
+}
+getDadosEstado()
+//console.log(getListaDeEstados())
