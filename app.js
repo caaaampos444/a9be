8c17607b401add29515c716fe19934342b13bd1a
caaaampos444 <gustavock18@hotmail.com>
2023-10-19 00:45:46 -0300

    exercicios 2 e 3 finalizados

--- app.js
+++ app.js
@@ -13,18 +13,41 @@ const getListaDeEstados=function(){
 }
 const getDadosEstado=function(){
     let JSONDados={}
-    let filtro='RJ'
+    let filtro='SP'
     let contador=0
-    /*loop: while(true){
-            if(filtro==estadosCidades.estados[contador].sigla){
-                JSONDados.uf=estadosCidades.estados[contador].sigla
-                JSONDados.descricao=estadosCidades.estados[contador].nome
-                JSONDados.capital=estadosCidades.estados[contador].capital
-                JSONDados.regiao=estadosCidades.estados[contador].regiao
-                break loop
-            }
-    }*/
-    
+    loop: while(true){
+        if(filtro==estadosCidades.estados[contador].sigla){
+            JSONDados.uf=estadosCidades.estados[contador].sigla
+            JSONDados.descricao=estadosCidades.estados[contador].nome
+            JSONDados.capital=estadosCidades.estados[contador].capital
+            JSONDados.regiao=estadosCidades.estados[contador].regiao
+            break loop
+        }
+        contador++
+    }
+    return JSONDados
 }
-//getDadosEstado()
-console.log(getListaDeEstados())
+const getCapitalEstado=function(){
+    let JSONCapital={}
+    let filtro='AC'
+    let contador=0
+    loop: while(true){
+        if(filtro==estadosCidades.estados[contador].sigla){
+            JSONCapital.uf=estadosCidades.estados[contador].sigla
+            JSONCapital.descricao=estadosCidades.estados[contador].nome
+            JSONCapital.capital=estadosCidades.estados[contador].capital
+            break loop
+        }
+        contador++
+    }
+    return JSONCapital
+}
+const getEstadosRegiao=function(){
+    let JSONRegiao={}
+    let arrayEstados=[]
+    let filtro='SUL'
+    let contador=0
+}
+//console.log(getCapitalEstado())
+//console.log(getDadosEstado())
+//console.log(getListaDeEstados())
