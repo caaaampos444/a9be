1041be43eff1f81857e2ad7d75f3f8c66b22328a
GUSTAVO DE CAMPOS <23111788@CFP127ALUNO.LOCAL>
2023-11-01 12:32:14 -0300

    api iniciada

--- app.js
+++ app.js
@@ -1,125 +1,30 @@
-const estadosCidades=require('./modulo/estados_cidades')
+/*********************************************************************************
+ * Objetivo: Criação de uma API para manipular dados de Estados e Cidades
+ * Data: 01/11/2023
+ * Autor: Gustavo Campos
+ * Versão: 1.0
+ *********************************************************************************/
 
-const getListaDeEstados=function(){
-    let JSONSiglas={}
-    let arraySiglas=[]
-    let contador=0
-    while(contador<27){
-        arraySiglas.push(estadosCidades.estados[contador].sigla)
-        contador++
-    }
-    JSONSiglas.uf=arraySiglas
-    JSONSiglas.quantidade=arraySiglas.length
-    return JSONSiglas
-}
+const express=require('express')
+const bodyParser=require('body-parser')
+const cors=require('cors')
 
-const getDadosEstado=function(){
-    let JSONDados={}
-    let filtro='SP'
-    let contador=0
-    while(true){
-        if(filtro==estadosCidades.estados[contador].sigla){
-            JSONDados.uf=estadosCidades.estados[contador].sigla
-            JSONDados.descricao=estadosCidades.estados[contador].nome
-            JSONDados.capital=estadosCidades.estados[contador].capital
-            JSONDados.regiao=estadosCidades.estados[contador].regiao
-            break
-        }
-        contador++
-    }
-    return JSONDados
-}
+const app=express()
 
-const getCapitalEstado=function(){
-    let JSONCapital={}
-    let filtro='AC'
-    let contador=0
-    while(true){
-        if(filtro.toUpperCase()==estadosCidades.estados[contador].sigla.toUpperCase()){
-            JSONCapital.uf=estadosCidades.estados[contador].sigla.toUpperCase()
-            JSONCapital.descricao=estadosCidades.estados[contador].nome
-            JSONCapital.capital=estadosCidades.estados[contador].capital
-            break
-        }
-        contador++
-    }
-    return JSONCapital
-}
+app.use((request,response,next)=>{
+    response.header('Access-Control-Allow-Origin','*')
+    response.header('Access-Control-Allow-Methods','GET')
+    app.use(cors())
+    next()
+})
 
-const getEstadosRegiao=function(){
-    let JSONRegiao={}
-    let arrayEstados=[]
-    let filtro='NORDESTE'
-    let contador=0
-    while(contador<27){
-        if(filtro.toUpperCase()==estadosCidades.estados[contador].regiao.toUpperCase()){
-            let JSONEstados={}
-            JSONEstados.uf=estadosCidades.estados[contador].sigla
-            JSONEstados.nome=estadosCidades.estados[contador].nome
-            arrayEstados.push(JSONEstados)
-        }
-        contador++
-    }
-    JSONRegiao.regiao=filtro.toUpperCase()
-    JSONRegiao.estados=arrayEstados
-    return JSONRegiao
-}
+app.get('/estados/sigla',cors(),async function(request,response,next){
+    let controleEstadosCidades=require('./modulo/funcoes.js')
+    let listaEstados=controleEstadosCidades.getListaDeEstados()
+    response.json(listaEstados)
+    response.status(200)
+})
 
-const getCapitalPais=function(){
-    let JSONCapitais={}
-    let arrayCapitais=[]
-    let contador=0
-    while(contador<27){
-        if(estadosCidades.estados[contador].capital_pais){
-            let JSONEstados={}
-            JSONEstados.capital_atual=estadosCidades.estados[contador].capital_pais.capital
-            JSONEstados.uf=estadosCidades.estados[contador].sigla
-            JSONEstados.descricao=estadosCidades.estados[contador].nome
-            JSONEstados.capital=estadosCidades.estados[contador].capital
-            JSONEstados.regiao=estadosCidades.estados[contador].regiao
-            JSONEstados.capital_pais_ano_inicio=estadosCidades.estados[contador].capital_pais.ano_inicio
-            JSONEstados.capital_pais_ano_termino=estadosCidades.estados[contador].capital_pais.ano_fim
-            arrayCapitais.push(JSONEstados)
-        }
-        contador++
-    }
-    JSONCapitais.capitais=arrayCapitais
-    return JSONCapitais
-}
-
-const getCidades=function(){
-    let JSONCidades={}
-    let arrayCidades=[]
-    let filtro='AC'
-    let contadorEstados=0
-    let contadorCidades=0
-    while(true){
-        if(filtro==estadosCidades.estados[contadorEstados].sigla){
-            while(arrayCidades.length<estadosCidades.estados[contadorEstados].cidades.length){
-                arrayCidades.push(estadosCidades.estados[contadorEstados].cidades[contadorCidades].nome)
-                contadorCidades++
-            }
-            break
-        }
-        contadorEstados++
-    }
-    contadorEstados=0
-    while(true){
-        if(filtro==estadosCidades.estados[contadorEstados].sigla){
-            JSONCidades.uf=estadosCidades.estados[contadorEstados].sigla
-            JSONCidades.descricao=estadosCidades.estados[contadorEstados].nome
-            JSONCidades.quantidade_cidades=estadosCidades.estados[contadorEstados].cidades.length
-            JSONCidades.cidades=arrayCidades
-            break
-        }
-        contadorEstados++
-    }
-    return JSONCidades
-}
-
-//console.log(getCidades())
-//console.log(getCapitalPais())
-//console.log(getEstadosRegiao())
-//console.log(getCapitalEstado())
-//console.log(getDadosEstado())
-//console.log(getListaDeEstados())
+app.listen('8080',function(){
+    console.log('API funcionando!!!')
+})
---
+++ modulo/funcoes.js
@@ -0,0 +1,126 @@
+const estadosCidades=require('./estados_cidades')
+
+const getListaDeEstados=function(){
+    let JSONSiglas={}
+    let arraySiglas=[]
+    let contador=0
+    while(contador<27){
+        arraySiglas.push(estadosCidades.estados[contador].sigla)
+        contador++
+    }
+    JSONSiglas.uf=arraySiglas
+    JSONSiglas.quantidade=arraySiglas.length
+    return JSONSiglas
+}
+
+const getDadosEstado=function(){
+    let JSONDados={}
+    let filtro='SP'
+    let contador=0
+    while(true){
+        if(filtro==estadosCidades.estados[contador].sigla){
+            JSONDados.uf=estadosCidades.estados[contador].sigla
+            JSONDados.descricao=estadosCidades.estados[contador].nome
+            JSONDados.capital=estadosCidades.estados[contador].capital
+            JSONDados.regiao=estadosCidades.estados[contador].regiao
+            break
+        }
+        contador++
+    }
+    return JSONDados
+}
+
+const getCapitalEstado=function(){
+    let JSONCapital={}
+    let filtro='AC'
+    let contador=0
+    while(true){
+        if(filtro.toUpperCase()==estadosCidades.estados[contador].sigla.toUpperCase()){
+            JSONCapital.uf=estadosCidades.estados[contador].sigla.toUpperCase()
+            JSONCapital.descricao=estadosCidades.estados[contador].nome
+            JSONCapital.capital=estadosCidades.estados[contador].capital
+            break
+        }
+        contador++
+    }
+    return JSONCapital
+}
+
+const getEstadosRegiao=function(){
+    let JSONRegiao={}
+    let arrayEstados=[]
+    let filtro='SUL'
+    let contador=0
+    while(contador<27){
+        if(filtro.toUpperCase()==estadosCidades.estados[contador].regiao.toUpperCase()){
+            let JSONEstados={}
+            JSONEstados.uf=estadosCidades.estados[contador].sigla
+            JSONEstados.nome=estadosCidades.estados[contador].nome
+            arrayEstados.push(JSONEstados)
+        }
+        contador++
+    }
+    JSONRegiao.regiao=filtro.toUpperCase()
+    JSONRegiao.estados=arrayEstados
+    return JSONRegiao
+}
+
+const getCapitalPais=function(){
+    let JSONCapitais={}
+    let arrayCapitais=[]
+    let contador=0
+    while(contador<27){
+        if(estadosCidades.estados[contador].capital_pais){
+            let JSONEstados={}
+            JSONEstados.capital_atual=estadosCidades.estados[contador].capital_pais.capital
+            JSONEstados.uf=estadosCidades.estados[contador].sigla
+            JSONEstados.descricao=estadosCidades.estados[contador].nome
+            JSONEstados.capital=estadosCidades.estados[contador].capital
+            JSONEstados.regiao=estadosCidades.estados[contador].regiao
+            JSONEstados.capital_pais_ano_inicio=estadosCidades.estados[contador].capital_pais.ano_inicio
+            JSONEstados.capital_pais_ano_termino=estadosCidades.estados[contador].capital_pais.ano_fim
+            arrayCapitais.push(JSONEstados)
+        }
+        contador++
+    }
+    JSONCapitais.capitais=arrayCapitais
+    return JSONCapitais
+}
+
+const getCidades=function(){
+    let JSONCidades={}
+    let arrayCidades=[]
+    let filtro='AC'
+    let contadorEstados=0
+    let contadorCidades=0
+    while(true){
+        if(filtro==estadosCidades.estados[contadorEstados].sigla){
+            while(arrayCidades.length<estadosCidades.estados[contadorEstados].cidades.length){
+                arrayCidades.push(estadosCidades.estados[contadorEstados].cidades[contadorCidades].nome)
+                contadorCidades++
+            }
+            break
+        }
+        contadorEstados++
+    }
+    contadorEstados=0
+    while(true){
+        if(filtro==estadosCidades.estados[contadorEstados].sigla){
+            JSONCidades.uf=estadosCidades.estados[contadorEstados].sigla
+            JSONCidades.descricao=estadosCidades.estados[contadorEstados].nome
+            JSONCidades.quantidade_cidades=estadosCidades.estados[contadorEstados].cidades.length
+            JSONCidades.cidades=arrayCidades
+            break
+        }
+        contadorEstados++
+    }
+    return JSONCidades
+}
+
+//console.log(getCidades())
+//console.log(getCapitalPais())
+//console.log(getEstadosRegiao())
+//console.log(getCapitalEstado())
+//console.log(getDadosEstado())
+console.log(getListaDeEstados())
+module.exports={getListaDeEstados}
